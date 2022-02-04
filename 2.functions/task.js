399c2448d0d11ef6be0a871fc5d1f88f10f8a827
Natalya Pletneva <nntalita@gmail.com>
2022-02-04 20:44:55 +0300

    fixed

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,26 +1,5 @@
 'use strict';
 
-// Задание 1
-function getArrayParams(arr) {
-    let min, max, sum, avg;
-    min = arr[0];
-    max = arr[0];
-    sum = 0;
-    avg = 0;
-    for (i = 0; i < arr.length; i++) {
-        if (arr[i] < min) {
-            min = arr[i]
-        }
-        ;
-        if (arr[i] > max) {
-            max = arr[i]
-        }
-        ;sum = sum + arr[i]
-    }
-    avg = +(sum / arr.length).toFixed(2);
-    return {min: min, max: max, avg: avg};
-}
-
 function getArrayParams(arr) {
     let min, max, sum, avg;
     min = arr[0];
@@ -31,11 +10,10 @@ function getArrayParams(arr) {
         if (arr[i] < min) {
             min = arr[i]
         }
-        ;
         if (arr[i] > max) {
             max = arr[i]
         }
-        ;sum = sum + arr[i]
+        sum = sum + arr[i];
     }
     avg = +(sum / arr.length).toFixed(2);
     return {min: min, max: max, avg: avg};
@@ -53,9 +31,10 @@ function worker(arr) {
 
 function makeWork(arrOfArr, func) {
     let max = func(arrOfArr[0]);
-    for (let i = 0; i < arrOfArr.length; i++) {
-        if (func(arrOfArr[i]) > max) {
-            max = func(arrOfArr[i])
+    for (let i = 1; i < arrOfArr.length; i++) {
+        let tmp = func(arrOfArr[i]);
+        if (tmp > max) {
+            max = tmp
         }
     }
     return max;
@@ -64,20 +43,6 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
-    let max, min, diff
-    let i = 0;
-    max = arr[0];
-    min = arr[0];
-    while (i < arr.length) {
-        if (arr[i] > max) {
-            max = arr[i]
-        }
-        ;
-        if (arr[i] < min) {
-            min = arr[i]
-        }
-        ;i++;
-    }
-    diff = Math.abs(max - min);
-    return diff;
+    let arrParams = getArrayParams(arr);
+    return Math.abs(arrParams.max - arrParams.min);
 }
