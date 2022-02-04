264b7a37f52d73f5b4e35a5bfd64a335fe1924f0
Natalya Pletneva <nntalita@gmail.com>
2022-02-04 13:21:04 +0300

    function homework task 1,2.3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,94 @@
+'use strict';
+
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+    let min, max, sum, avg;
+    min = arr[0];
+    max = arr[0];
+    sum = 0;
+    avg = 0;
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+        ;
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+        ;
+        sum = sum + arr[i]
+    }
+    avg = +(sum / arr.length).toFixed(2);
 
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+    return {min: min, max: max, avg: avg};
 }
 
-// Задание 2
+
+function getArrayParams(arr) {
+    let min, max, sum, avg;
+    min = arr[0];
+    max = arr[0];
+    sum = 0;
+    avg = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+        ;
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+        ;
+        sum = sum + arr[i]
+    }
+    avg = +(sum / arr.length).toFixed(2);
+
+    return {min: min, max: max, avg: avg};
+}
+
 function worker(arr) {
-  let sum;
+    let sum = 0;
+    let i = 0;
+    while (i < arr.length) {
 
-  // Ваш код
-
-  return sum;
+        sum = sum + arr[i];
+        i++;
+    }
+    return sum;
 }
+
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max = func(arrOfArr[0]);
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+
+    for (let i = 0; i < arrOfArr.length; i++) {
+        if (func(arrOfArr[i]) > max) {
+
+            max = func(arrOfArr[i])
+        }
+    }
+    return max;
 }
+
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    let max, min, diff
+    let i = 0;
+    max = arr[0];
+    min = arr[0];
+    while (i < arr.length) { // выводит 0, затем 1, затем 2
+        if (arr[i] > max) {
+            max = arr[i]
+        }
+        ;
+        if (arr[i] < min) {
+            min = arr[i]
+        }
+        ;
+        i++;
+    }
+    diff = Math.abs(max - min);
+    return diff;
 }
