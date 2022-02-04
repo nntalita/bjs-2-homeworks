b21355048fed8a2ec8779d50724d3ddae708687c
Natalya Pletneva <nntalita@gmail.com>
2022-02-04 13:31:09 +0300

    format

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -15,14 +15,11 @@ function getArrayParams(arr) {
         if (arr[i] > max) {
             max = arr[i]
         }
-        ;
-        sum = sum + arr[i]
+        ;sum = sum + arr[i]
     }
     avg = +(sum / arr.length).toFixed(2);
-
     return {min: min, max: max, avg: avg};
 }
-
 
 function getArrayParams(arr) {
     let min, max, sum, avg;
@@ -38,11 +35,9 @@ function getArrayParams(arr) {
         if (arr[i] > max) {
             max = arr[i]
         }
-        ;
-        sum = sum + arr[i]
+        ;sum = sum + arr[i]
     }
     avg = +(sum / arr.length).toFixed(2);
-
     return {min: min, max: max, avg: avg};
 }
 
@@ -50,21 +45,16 @@ function worker(arr) {
     let sum = 0;
     let i = 0;
     while (i < arr.length) {
-
         sum = sum + arr[i];
         i++;
     }
     return sum;
 }
 
-
 function makeWork(arrOfArr, func) {
     let max = func(arrOfArr[0]);
-
-
     for (let i = 0; i < arrOfArr.length; i++) {
         if (func(arrOfArr[i]) > max) {
-
             max = func(arrOfArr[i])
         }
     }
@@ -78,7 +68,7 @@ function worker2(arr) {
     let i = 0;
     max = arr[0];
     min = arr[0];
-    while (i < arr.length) { // выводит 0, затем 1, затем 2
+    while (i < arr.length) {
         if (arr[i] > max) {
             max = arr[i]
         }
@@ -86,8 +76,7 @@ function worker2(arr) {
         if (arr[i] < min) {
             min = arr[i]
         }
-        ;
-        i++;
+        ;i++;
     }
     diff = Math.abs(max - min);
     return diff;
