0cd74551d22fd5cd7c40c1407e06da7e672560ce
Natalya Pletneva <nntalita@gmail.com>
2022-02-08 22:46:48 +0300

    arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
+`use strict`
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+    let result;
+    return (arr2.every(function (element, index) {
+        return arr1[index] === arr2[index];
+    })) && (arr1.every(function (element, index) {
+        return arr1[index] === arr2[index];
+    }));
+    return result; // boolean
 }
 
+
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+    let resultArr = arr.filter(function (number) {
+        return (number > 0) && (number % 3 === 0);
+    }).map(function (number) {
+        return number * 10
+    });
+    return resultArr;
 }
