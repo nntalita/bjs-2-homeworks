675019bd959706ebc16207a859982275584b7ba1
Natalya Pletneva <nntalita@gmail.com>
2022-02-14 20:17:07 +0300

    Objects task

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+    this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+    if (this.marks === undefined) {
+        this.marks = [mark]
+    } else {
+        this.marks.push(mark)
+    }
+}
+
+Student.prototype.addMarks = function (...mark) {
+    for (let item of mark) {
+        this.addMark(item)
+    }
+
+}
+Student.prototype.getAverage = function () {
+    let result = this.marks.reduce( (sum, current) => {
+        return sum + current
+    });
+    result = result / this.marks.length;
+    return result;
+}
+
+Student.prototype.exclude = function (reason) {
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
+}
