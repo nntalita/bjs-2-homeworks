9f1c99e7bee4a82adb6138f68e1b2cbd9a802587
Natalya Pletneva <nntalita@gmail.com>
2022-02-28 23:20:47 +0300

    decorator

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,71 @@
+`use strict`
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+
+    function wrapper(...args) {
+        let hash = args.join(',');
+        let index = cache.findIndex(item => item.hash === hash);
+        if (index !== -1) {
+            console.log(("Из кэша: " + cache[index].value));
+            return ("Из кэша: " + cache[index].value);
+        } else {
+            let result = func(...args);
+            let item = {hash, value: result};
+            cache.push(item);
+            if (cache.length > 5) {
+                cache.shift()
+            }
+            console.log("Вычисляем: " + result);
+            return ("Вычисляем: " + result);
+        }
+    }
+
+    return wrapper
 }
 
+function debounceDecoratorNew(func, ms) {
+    let flag = false;
+    let timeout;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+    function wrapper(...arg) {
+        if (flag === false) {
+            func(...arg);
+            flag = true;
+            timeout = setTimeout(() => {
+                flag = false
+            }, ms)
+        } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                func(...arg);
+                flag = false;
+            }, ms)
+        }
+    }
+    return wrapper;
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+function debounceDecorator2(func, ms) {
+    let flag = false;
+    let timeout;
+    function wrapper(...arg) {
+        wrapper.count++;
+        console.log("Вызов функции номер " + wrapper.count);
+        if (flag === false) {
+            func(...arg);
+            flag = true;
+            timeout = setTimeout(() => {
+                flag = false
+            }, ms);
+        } else {
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                func(...arg);
+                flag = false;
+            }, ms);
+        }
+    }
+    wrapper.count = 0;
+    return wrapper;
 }
